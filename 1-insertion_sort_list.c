9d52b752ef04ed129ceb28d42b34740bffdb9b57
Iyanu456 <oyerindei13@gmail.com>
2023-01-24 12:12:24 -0800

    default message

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,5 @@
 #include "sort.h"
-void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t *h)
+void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h)
 {
 	while (pn && temp->n < pn->n)
 	{
@@ -8,17 +8,17 @@ void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t *h)
 		temp->next = pn;
 		temp->prev = pn->prev;
 		if (pn->prev)
-			(pn-prev)->next = temp;
+			(pn->prev)->next = temp;
 		pn->prev = temp;
 		pn->next = nxt;
 		nxt = pn;
 		pn = temp->prev;
 		if ((*h)->prev)
 			*h = (*h)->prev;
-		print_list(h);
+		print_list(*h);
 	}
 }
-void insertion_sor_lit(listint_t **list)
+void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp, *pn, *nxt;
 
@@ -32,7 +32,7 @@ void insertion_sor_lit(listint_t **list)
 		{
 			if (temp->n < pn->n)
 				swap_list(pn, temp, nxt, list);
-			tmp = nxt;
+			temp = nxt;
 			if (temp)
 			{
 				nxt = temp->next;
